f86a165117cfc4d141814d91a1ae7910e3505ff9
javier perez osorio <japere@bancolombia.com.co>
2023-09-18 11:36:28 -0500

    feat : todo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoModule } from './todos/todo.module';
+import { FooterComponent } from './footer/footer/footer.component';
+import { todoReducer } from './todos/todo.reducer';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TodoModule,
+    StoreModule.forRoot({ todos: todoReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todos/todo.reducer.ts
@@ -0,0 +1,15 @@
+import { createReducer, on } from '@ngrx/store';
+import { crear } from './todo.actions';
+import { Todo } from './models/todo.model';
+
+export const initialState:Todo[] = [];
+
+export const _todoReducer= createReducer(
+  initialState,
+  on(crear, (state,{texto})=> [...state, new Todo(texto)]),
+);
+
+
+export function todoReducer(state, action){
+  return _todoReducer(state,action);
+}
